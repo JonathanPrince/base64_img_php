6ca85494a66384b2fa371aade3d1c52132675bb0
Jonathan Prince <jonathan.prince@gmail.com>
2015-01-19 19:49:32 +0100

    detect input type

--- b64img.php
+++ b64img.php
@@ -1,6 +1,24 @@
 <?php
+
 class B64img {
-  public function __construct() {
-  }
+
+    public function __construct($input) {
+
+        // check if input is a valid file
+        if (file_exists($input)) {
+
+
+        } else if (strpos($input, 'data:') == 0) {
+
+
+        } else {
+
+
+        }
+
+    }
+
+
 }
+
 ?>
