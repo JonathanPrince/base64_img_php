a9215c5720cadd5367ecc7703d52841cb4b82a98
Jonathan Prince <jonathan.prince@gmail.com>
2015-01-19 20:17:28 +0100

    check input file is an image and encode data URI

--- b64img.php
+++ b64img.php
@@ -7,12 +7,44 @@ class B64img {
         // check if input is a valid file
         if (is_file($input)) {
 
+            $this->file_type = mime_content_type($input);
+
+            // check if file is an image before encoding
+            if ($this->is_image()) {
+
+                $file_contents = file_get_contents($input);
+
+                $this->data_URI = 'data:' . $this->file_type . ';base64,' . base64_encode($file_contents);
+
+            }
 
         } else if (strpos($input, 'data:') == 0) {
 
 
         } else {
 
+
+        }
+
+    }
+
+    /**
+    *   checks mime type of file
+    *   @return boolean true if mime type is an image
+    */
+
+    private function is_image() {
+
+        switch ($this->file_type) {
+
+            case 'image/jpeg':
+            case 'image/pjpeg':
+            case 'image/png':
+            case 'image/gif':
+                return true;
+
+            default:
+                return false;
 
         }
 
